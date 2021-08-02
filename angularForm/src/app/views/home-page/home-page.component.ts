aee1a750f6530d652b0e6d8fcc621853ae6a72b9
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-08-03 03:31:41 +0400

    building form based on json

--- angularForm/src/app/views/home-page/home-page.component.ts
+++ angularForm/src/app/views/home-page/home-page.component.ts
@@ -1,6 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import {
+  FormsModule,
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { FirebaseClientService } from './../../services/firebase-client.service';
+import testJson from './../../../assets/myForm.json';
+interface Root {
+  type: string;
+  name: string;
+  label: string;
+  properties: Property[];
+}
+
+interface Property {
+  type: string;
+  name: string;
+  label: string;
+  required?: boolean;
+  inputType?: string;
+  options?: Option[];
+  minLength?: number;
+  maxLength?: number;
+  pattern?: string;
+  integer?: boolean;
+  minimum?: number;
+  maximum?: number;
+  item?: Item;
+  multiline?: boolean;
+  properties?: Property3[];
+}
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface Item {
+  type: string;
+  name: string;
+  properties: Property2[];
+}
+
+interface Property2 {
+  type: string;
+  name: string;
+  label: string;
+  required: boolean;
+  integer?: boolean;
+}
+
+interface Property3 {
+  type: string;
+  name: string;
+  label: string;
+  required: boolean;
+}
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -8,24 +67,57 @@ import { FirebaseClientService } from './../../services/firebase-client.service'
   providers: [FirebaseClientService],
 })
 export class HomePageComponent implements OnInit {
-  schemaObject!: string;
+  clickedGenerate: boolean = false;
+  schemaObject!: Root;
+  testForm: Root = testJson;
+  myForm: FormGroup = this.fb.group({});
   constructor(
     private firebase: FirebaseClientService,
     private fb: FormBuilder
   ) {}
-  ngOnInit(): void {}
-  generateForm() {
-    let object = {};
-    const name = document.getElementById('displayName') as any;
-    if (this.readObjectValid(object)) {
-      name.innerHTML = 'valid';
-    } else {
-      name.innerHTML = 'is not valid';
-    }
+  ngOnInit(): void {
+    console.log(this.testForm);
   }
-  readObjectValid(object: Object) {
-    let isValid = false;
-    //test
-    return isValid;
+  createForm() {
+    this.clickedGenerate = true;
+    this.generateForm(this.testForm.properties);
+  }
+  generateForm(properties: Property[]) {
+    for (const property of properties) {
+      const validatorsToAdd = [];
+      for (const [key, value] of Object.entries(property)) {
+        switch (key) {
+          case 'minLength':
+            validatorsToAdd.push(Validators.minLength(value));
+            break;
+          case 'maxLength':
+            validatorsToAdd.push(Validators.maxLength(value));
+            break;
+          case 'required':
+            validatorsToAdd.push(Validators.required);
+            break;
+          case 'pattern':
+            validatorsToAdd.push(Validators.pattern(value));
+            break;
+          case 'maximum':
+            validatorsToAdd.push(Validators.max(value));
+            break;
+          case 'minimum':
+            validatorsToAdd.push(Validators.min(value));
+            break;
+          case 'inputType':
+            if (value == 'email') validatorsToAdd.push(Validators.email);
+            break;
+          default:
+            break;
+        }
+      }
+      this.myForm.addControl(
+        property.name,
+        this.fb.control('', validatorsToAdd)
+      );
+    }
+    console.log('form valid:', this.myForm.valid);
+    console.log('form values:', this.myForm.value);
   }
 }
